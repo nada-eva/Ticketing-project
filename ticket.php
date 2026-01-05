900b3024124c886607994f4a1873371620b6dccf
yasser <yasseramil58@gmail.com>
2026-01-05 22:45:30 +0100

    add can2025 project

---
+++ ticket.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>CAN 2025 - Accueil</title>
+    <link rel="stylesheet" href="style.css">
+      
+</head>
+<body>
+
+<header class="header">
+    <div class="logo">logo</div>
+    <nav>
+     
+        <a href="signup.php">Deconnection</a>
+        
+    </nav>
+</header>
+
+<main class="container">
+    <h1>Latest Tickets</h1>
+
+    <div class="cards">
+
+        <div class="card">
+            <img src="images/slide1.png" alt="Match 1">
+            <h3>Match 1 Maroc vs Comros</h3>
+            <p>Prix : 150 DH</p>
+            <a class="btn" href="acheter_ticket.php">Détails</a>
+        </div>
+
+        <div class="card">
+            <img src="images/slide2.png" alt="Match 2">
+            <h3>Match 2 Maroc vs Mali</h3>
+            <p>Prix : 150 DH</p>
+            <a class="btn" href="acheter_ticket.php">Détails</a>
+        </div>
+
+        <div class="card">
+            <img src="images/slide3.png" alt="Match 3">
+            <h3>Match 3 Maroc vs Zambie</h3>
+            <p>Prix : 150 DH</p>
+            <a class="btn" href="acheter_ticket.php">Détails</a>
+        </div>
+
+    </div>
+</main>
+
+
+
+</body>
+</html>
